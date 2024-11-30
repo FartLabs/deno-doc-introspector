389ef7aaf4969dd5609751e831c10a593f7f03b3
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-11-29 20:49:51 -0800

    Update typebox.ts

--- lib/typebox.ts
+++ lib/typebox.ts
@@ -1,3 +1,7 @@
 // TODO:
 // https://github.com/sinclairzx81/typebox-codegen/blob/7a859390ab29032156d8da260038b45cf63fc5a4/src/typescript/typescript-to-typebox.ts
 //
+
+// TODO: Test against select files from:
+// https://github.com/microsoft/TypeScript/blob/main/tests/cases/compiler
+//
