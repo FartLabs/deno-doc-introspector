b414bf2a4c5774985f57fa405c75b5ad89dc1a68
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-11-30 10:28:21 -0800

    create task `get-tests`

---
+++ tests.ts
@@ -0,0 +1,24 @@
+import { exists } from "@std/fs/exists";
+
+const remoteAddress =
+  "https://github.com/microsoft/TypeScript/raw/b263cc4b2ef12ae013526a3d8808b6716146586a/tests/cases/compiler";
+
+const testCases = [
+  "2dArrays.ts",
+];
+
+const testsDirectory = "tests";
+
+if (import.meta.main) {
+  if (await exists(testsDirectory)) {
+    await Deno.remove(testsDirectory, { recursive: true });
+  }
+
+  await Deno.mkdir(testsDirectory);
+  for (const testCase of testCases) {
+    const url = `${remoteAddress}/${testCase}`;
+    const response = await fetch(url);
+    const text = await response.text();
+    await Deno.writeTextFile(`${testsDirectory}/${testCase}`, text);
+  }
+}
