9f828ad60f0973d23c02bdcd459ab3a385e3e5d6
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-12-28 21:55:10 -0800

    Delete typebox.ts

--- lib/typebox.ts
+++
@@ -1,7 +0,0 @@
-// TODO:
-// https://github.com/sinclairzx81/typebox-codegen/blob/7a859390ab29032156d8da260038b45cf63fc5a4/src/typescript/typescript-to-typebox.ts
-//
-
-// TODO: Test against select files from:
-// https://github.com/microsoft/TypeScript/blob/main/tests/cases/compiler
-//
